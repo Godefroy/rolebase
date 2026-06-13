598829ca4bea414dd133905192880595427cc9a8
Godefroy <git@skreo.net>
2026-06-14 00:09:35 +0200

    fix(webapp): adapt CircleCard to CircleBreadcrumb and heading title

--- packages/webapp/share/CircleCard.tsx
+++ packages/webapp/share/CircleCard.tsx
@@ -1,8 +1,10 @@
-import CircleAndParentsLinks from '@/circle/components/CircleAndParentsLinks'
+import CircleBreadcrumb from '@/circle/components/CircleBreadcrumb'
+import CircleByIdButton from '@/circle/components/CircleByIdButton'
 import CircleRole from '@/circle/components/CircleRole'
 import { CircleContext } from '@/circle/contexts/CIrcleContext'
-import { Box, ModalCloseButton } from '@chakra-ui/react'
+import { Box, Flex, Heading, ModalCloseButton } from '@chakra-ui/react'
 import React, { useContext } from 'react'
+import { CircleParentLinkIcon } from 'src/icons'
 
 interface Props {
   id: string
@@ -17,7 +19,20 @@ export default function CircleCard({ id }: Props) {
   return (
     <Box p={5}>
       <ModalCloseButton />
-      <CircleAndParentsLinks circle={circle} size="md" mb={5} />
+      <CircleBreadcrumb circleId={circle.id} mb={2} />
+      <Flex alignItems="center" gap={3} mb={5}>
+        <Heading as="h1" size="md" fontWeight="bold">
+          {circle.role.name}
+        </Heading>
+        {circle.role.parentLink && circle.parentId && (
+          <>
+            <Box color="gray.500" _dark={{ color: 'gray.300' }}>
+              <CircleParentLinkIcon size="1.5em" />
+            </Box>
+            <CircleByIdButton id={circle.parentId} size="md" />
+          </>
+        )}
+      </Flex>
       <CircleRole skipFetchRole />
     </Box>
   )
